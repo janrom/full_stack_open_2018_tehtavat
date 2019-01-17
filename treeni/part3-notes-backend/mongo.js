6893bb866a9cf0cb0f372e1bb7d4c145d9a8177d
Janne Romppanen <janneromppanen@fastmail.com>
2019-01-17 12:05:24 +0200

    MongoDB harjoitus tehtavaan 3-12 asti

--- treeni/part3-notes-backend/mongo.js
+++ treeni/part3-notes-backend/mongo.js
@@ -6,26 +6,44 @@ data = data.split(':')
 const usr = data[1]
 const pass = data[3].substring(0, data[3].length - 1)
 
-// yhteys cLabissa olevaan tietokantaan notes
+// create url to MongoDB database notes in mLab
 const url = 'mongodb://' + usr + ':' + pass + '@ds157834.mlab.com:57834/notes'
 
+// open connection to database
 mongoose.connect(url)
 
+// create a model. schema is defined in second parameter. 
 const Note = mongoose.model('Note', {
   content: String,
   date: Date,
   important: Boolean
 })
 
+// create a document from the model
 const note = new Note({
   content: 'testi',
   date: new Date(),
   important: true
 })
 
+// save the document into collection notes
+/*
 note
   .save()
   .then(response => {
-    console.log('note saved')
+    console.log('note saved', note)
+    // close connection to database
     mongoose.connection.close()
   })
+*/
+
+// get all documents from database
+Note
+  .find({})
+  .then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+
