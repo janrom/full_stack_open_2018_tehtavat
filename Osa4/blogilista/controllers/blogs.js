f917f5c9290926657b80e1e20cca0a7a72d398c7
Janne Romppanen <janneromppanen@fastmail.com>
2019-02-08 19:38:23 +0200

    bloglist. refactored post route to handle saving of single object and array of objects

--- Osa4/blogilista/controllers/blogs.js
+++ Osa4/blogilista/controllers/blogs.js
@@ -9,20 +9,44 @@ blogsRouter.get('/', (request, response) => {
     })
 })
 
-blogsRouter.post('/', (request, response) => {  
-  if (!request.body.title || !request.body.url) {
-    return response.status(400).json({ error: 'title or url missing' })
-  }  
-  if (!request.body.likes) {
-    request.body.likes = 0
+/**
+ * Save array of blog(s) to database
+ * @param {object} request - request.body holds single blog or array of blogs to be saved in database
+ * @param {object} response - HTTP response with JSON, holding created blog documents or error message
+ */
+blogsRouter.post('/', (request, response) => {
+  // handle only arrays. push single blog objects to array
+  let blogData = []
+  if (!request.body.length) {
+    blogData.push(request.body)
+  } else {
+    blogData = request.body
   }
-  const blog = new Blog(request.body)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const createdBlogs = blogData.map(blog => {
+    // mandatory fields
+    if (!blog.title || !blog.url) {
+      return response.status(400).json({ error: 'title or url missing' })
+    }
+
+    // if likes field is missing, add default field
+    if (!blog.likes) {
+      blog.likes = 0
+    }
+
+    // save blog document
+    const blogDocument =  new Blog(blog)
+    blogDocument
+      .save()
+      .catch(err => {
+        console.log(err)
+        return response(500).json({ error: 'failed to save blog document in POST /api/blogs' })
+      })
+
+    return blogDocument
+  })
+
+  response.status(201).json(createdBlogs)
 })
 
 module.exports = blogsRouter
